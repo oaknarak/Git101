59595bcdf89c13a1543e1a47ef203e20540861ff
oaknarak <Panithi.s@kkumail.com>
2023-08-15 18:10:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <style>
+        .material-symbols-outlined {
+        font-variation-settings:
+        'FILL' 0,
+        'wght' 400,
+        'GRAD' 0,
+        'opsz' 48
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row pt-4">
+            <div class="col-2 col-xl-3">
+                <ul>
+                    <a href=""><li>Overviwe</li></a>
+                    <a href=""><li>Echart</li></a>
+                    <a href=""><li>Profile</li></a>
+                    <a href=""><li>Mailbox</li></a>
+                </ul>
+            </div>
+            <div class="col-10 col-xl-9">
+                <div class="container">
+                    <div class="row mb-5 align-center">
+                        <div class="col-8">
+                            <div class="p">
+                                <h3>Dashboard</h3>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <span class="material-symbols-outlined">
+                                notifications
+                            </span>
+                            <span class="material-symbols-outlined">
+                                chat_bubble
+                            </span>
+                        </div>
+                        <div class="col-2">
+                            <div class="p">
+                                <h3>Panithi</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row-gap-4">
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Bitcoin</h4>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">$48,76950</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Ethereum</h4>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">$50,000</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Solana</h4>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">$48,769</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Price</h4>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">The price of bitcoin over the last day</h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                        <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Market Status</h4>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">detail</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row pt-5">
+                        <div class="p">
+                            <h5>USD Price Exchange</h5>
+                        </div>
+                        <div class="col-8">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">detail</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Market Analytics</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+</script>
+
+</html>
